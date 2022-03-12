f5ac1905d541cacfff9f03655cafc1539bd402bd
jai-kumar <jaikumar.mnnit@gmail.com>
2022-03-12 08:10:52 -0800

    added logs in cron service

--- services/cron.service.js
+++ services/cron.service.js
@@ -1,8 +1,10 @@
 module.exports = {
 	cronObj: {
 		start: function (cb, countryCode) {
+			console.log(`cron started`);
 			let count = 0;
 			setInterval(() => {
+				console.log(`calling cb from cron on: ${Date.now()} with country: ${countryCode}`);
 				if (countryCode) {
 					cb(count, countryCode);
 				} else {
@@ -15,8 +17,10 @@ module.exports = {
 			}, 900000);
 		},
 		start7Times: function (cb, countryCode, finalCb) {
+			console.log(`cron 7 times started`);
 			let count = 0;
 			let intervalId = setInterval(() => {
+				console.log(`calling cb from crom start7Times on: ${Date.now()} with country: ${countryCode}`);
 				if (countryCode) {
 					cb(count, countryCode);
 				} else {
